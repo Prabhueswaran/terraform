a42c05628c4ac874aa607339e0adb8e8f19c56f6
prabhueswaran <prabhueswaran322410@gmail.com>
2025-04-21 10:27:13 +0530

    Updated s3 verion block

--- AWS/projects/dev_account/ap-south-1/s3.tf
+++ AWS/projects/dev_account/ap-south-1/s3.tf
@@ -1,17 +1,25 @@
-# resource "aws_s3_bucket" "my_s3_bucket" {
-#   bucket = "my-tf-devops-bucket-2024"
-#   tags = {
-#     name        = "project1"
-#     environment = "dev"
-#   }
-#   force_destroy       = false
-#   object_lock_enabled = true
-# }
-
-module "s3" {
-  source = "../../../../AWS/modules/S3/"
-  bucket_name = local.bucket_name
-  force_destroy = false
-  object_lock_enabled = false
-  tag = [ "dev", "project1" ]
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-tf-devops-bucket-2025"
+  tags = {
+    name        = "project1"
+    environment = "dev"
+  }
+  force_destroy       = true
+  object_lock_enabled = true
 }
+
+resource "aws_s3_bucket_versioning" "name" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+# module "s3" {
+#   source = "../../../../AWS/modules/S3/"
+#   bucket_name = local.bucket_name
+#   force_destroy = false
+#   object_lock_enabled = false
+#   tag = [ "dev", "project1" ]
+# }
